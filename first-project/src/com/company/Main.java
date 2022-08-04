1a6e60cae9b76b7938889817ebc893141067b1d3
Fabian Morales Astorga <fmorales.astorga.15@gmail.com>
2022-08-04 14:50:40 -0400

    first project java

---
+++ first-project/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+        printDataTypes();
+    }
+
+    private static void printDataTypes() {
+        System.out.println("Los tipos de datos son: Enteros, Punto Flotante, Caracter, Booleanos, cadenas de Texto, Envoltorio");
+    }
+}
