49061931dc7212565b0fbb683f55f8809a702cfe
Fabian Morales Astorga <fmorales.astorga.15@gmail.com>
2022-08-04 22:34:22 -0400

    update

---
+++ first-project/src/com/company/control.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class control {
+    public static void main(String[] args) {
+        String[] text = new String[]{"lala", "perra", "lea"}
+        concatenacion(text);
+    }
+
+    private static string concatenacion(n) {
+        String concat = null;
+        for (int i=0;i<n.lenght; i++ ){
+            concat = n[i];
+        }
+
+    }
+
+}
